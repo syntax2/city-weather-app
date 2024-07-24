4228de8ff5e8ecdd953aedc944381a24ca20cbef
syntax2 <akadian135@gmail.com>
2024-07-24 23:39:29 +0530

    created a new component for the app

---
+++ src/app/city-search/city-search.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-city-search',
+  standalone: true,
+  imports: [],
+  templateUrl: './city-search.component.html',
+  styleUrl: './city-search.component.css'
+})
+export class CitySearchComponent {
+
+}
