89c159edcdaf9903de2e4c2b79ffcb7abd3fbdcc
Syntax2 <akadian135@gmail.com>
2024-07-25 23:08:46 +0530

    added code for the serching form and added other comp

--- src/app/city-search/city-search.component.ts
+++ src/app/city-search/city-search.component.ts
@@ -1,12 +1,29 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+
+import { MatButtonModule } from '@angular/material/button';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatIconModule } from '@angular/material/icon';
+import { MatInputModule } from '@angular/material/input';
+import { FormControl } from '@angular/forms';
+import { from } from 'rxjs';
+
 
 @Component({
   selector: 'app-city-search',
   standalone: true,
-  imports: [],
+  imports: [FormsModule,
+    ReactiveFormsModule,
+    MatFormFieldModule,
+    MatInputModule],
   templateUrl: './city-search.component.html',
   styleUrl: './city-search.component.css'
 })
-export class CitySearchComponent {
+export class CitySearchComponent implements OnInit {
+  ngOnInit(): void {
+    throw new Error('Method not implemented.');
+  }
+
+  search = new FormControl()
 
 }
